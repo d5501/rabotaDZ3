44bcbfd3141a596e88f7fb10c1d0603ba210f43c
Dmitriy Borisenko <dimas456654@mail.ru>
2023-06-27 22:18:35 +0300

    семинар7

---
+++ семинар7/seminar7.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3  -> 11
+// 2  -> 10
+
+Console.WriteLine("введите число");
+int number = Convert.ToInt32(Console.ReadLine()); 
+int i = 0;
+string a  ="";
+
+while(number !=0)
+{
+    i = number % 2;
+    number = number / 2;
+    a = a + i;
+}
+Console.WriteLine(a);
