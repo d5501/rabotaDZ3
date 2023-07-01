6897172b57c939bed88291407c77c6f7d662b5ee
Dmitriy Borisenko <dimas456654@mail.ru>
2023-07-02 00:11:48 +0300

    zada1DZ3

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+    {
+        Console.Write("введите число:");
+        var n = Convert.ToInt32(Console.ReadLine());
+        var sum = 0;
+        while(n > 0)
+        {
+            sum += n % 10;
+            n /= 10;
+        }
+        Console.WriteLine(sum);
+    }
